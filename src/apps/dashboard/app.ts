560262a235688a73bf7bcb93546bbcfe73771f36
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-24 15:54:11 +0100

    Make esmsh be proxied form the server and allow to customize esm sh endpoin
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/apps/dashboard/app.ts
+++ src/apps/dashboard/app.ts
@@ -2,6 +2,7 @@ import Koa from "koa";
 import basicAuth from "koa-basic-auth";
 import config from "config";
 import koaStatic from "koa-static";
+import proxy from "koa-proxies";
 
 const { username, password } = config.get<{ username: string; password: string }>("apps.dashboard.basicAuth");
 
@@ -10,7 +11,13 @@ export const app = () => {
 
   koa.use(basicAuth({ name: username, pass: password }));
   koa.use(koaStatic("./src/apps/dashboard/public"));
-  koa.use(koaStatic("./node_modules"));
+  koa.use(
+    proxy("/deps", {
+      target: config.get("apps.dashboard.esmsh"),
+      rewrite: (path) => path.replace("/deps", ""),
+      changeOrigin: true,
+    }),
+  );
 
   return koa;
 };
